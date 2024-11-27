efe1886cdc2475a5b7081bb6951f76aa41ec08b9
Rafiya Rehan <rafiya.21rehan@gamil.com>
2024-11-27 15:29:00 +0500

    Added SEO meta tags to the homepage

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { getPostData } from '../lib/posts';
+import Head from 'next/head';
 
 // Using getStaticProps for Static Site Generation
 export async function getStaticProps() {
@@ -13,10 +14,18 @@ export async function getStaticProps() {
 
 export default function Home({ data, content }) {
   return (
-    <div>
-      <h1>{data.title}</h1>
-      <p>{data.date}</p>
-      <div dangerouslySetInnerHTML={{ __html: content }} />
-    </div>
+    <>
+      <Head>
+        <title>{data.title}</title>  {/* Dynamic title from Markdown */}
+        <meta name="description" content={data.title} /> {/* Description from title */}
+        <meta property="og:title" content={data.title} /> {/* Open Graph title */}
+        <meta property="og:description" content={data.title} /> {/* Open Graph description */}
+      </Head>
+      <div>
+        <h1>{data.title}</h1>
+        <p>{data.date}</p>
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+      </div>
+    </>
   );
 }
